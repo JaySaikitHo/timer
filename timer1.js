11fb6ed5f3da19ce7e007c5a233bc8f270e644f9
vagrant <ttjho@hotmail.com>
2020-09-23 01:31:53 +0000

    made simple timer with callback

--- timer1.js
+++ timer1.js
@@ -1,1 +1,15 @@
+let input = process.argv.slice(2).sort();
+input = input.filter(num => num > 0);
 
+console.log(input)
+const timer = function (){
+  
+
+
+  for(let i = 0; i < input.length; i++) {
+     
+    setTimeout(() => { process.stdout.write('\x07')}, input[i]*1000)
+  }
+}
+
+timer();
